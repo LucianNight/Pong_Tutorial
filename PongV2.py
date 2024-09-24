b05d4dc6cab6103da229cdf4dc1fc1d7a05fe615
LucianNight <167735580+LucianNight@users.noreply.github.com>
2024-09-23 20:59:45 -0400

    Add files via upload
    This what we completed on the first day with some additional commenting

--- PongV2.py
+++ PongV2.py
@@ -4,7 +4,7 @@ import pygame
 pygame.init()
 clock = pygame.time.Clock()
 
-
+#define screen variables
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
@@ -35,7 +35,9 @@ class Player:
         SPEED = 10
         
         key = pygame.key.get_pressed()
-
+        #checks for which player it is first
+        #then for which button is pressed and determines
+        #positive and negative speed depending (top of the screen = 0)
         if self.player == 1:
             if key[pygame.K_w]:
                 vel_y = -SPEED
@@ -48,16 +50,15 @@ class Player:
                 vel_y = SPEED
             
             
-
+        #keeps the players within the bounds of the screen
         if self.rect.top + vel_y <= 0:
             vel_y = -self.rect.top
         if self.rect.bottom + vel_y >= SCREEN_HEIGHT:
             vel_y = SCREEN_HEIGHT - self.rect.bottom
-            
+
+        #updates the rectangle of the player from the velocity of y    
         self.rect.y += vel_y
         
-        
-
     def draw(self, surface):
         pygame.draw.rect(surface, self.color, self.rect)
 
@@ -77,14 +78,19 @@ class Ball:
 
     def move(self, player_one, player_two):
 
+        #if the ball hits the bottom of the screen or the top of the screen
+        #it "bounces" and changes directions
         if self.rect.bottom >= SCREEN_HEIGHT:
             self.vel_y *= -1
         if self.rect.top <= 0:
             self.vel_y *= -1
 
+        #Checks for collision with either of the paddles
         if self.rect.colliderect(player_one) or self.rect.colliderect(player_two):   
             self.vel_x *= -1
 
+        #If the ball goes off the screen it resets to let the
+        #losing player serve
         if self.rect.left >= SCREEN_WIDTH:
             self.pos_x = player_two.rect.left - self..radius  
             self.pos_y = player_two.rect.centery
@@ -97,38 +103,48 @@ class Ball:
             self.vel_x = 7
             self.vel_y = 7
             
-
+        #Updated the ball position x and y with the change in velocities
         self.pos_y += self.vel_y
         self.pos_x += self.vel_x
 
+        #updates the rectangle of the ball
         self.rect.center = (self.pos_x,self.pos_y)
 
     
     def draw(self, surface):
         pygame.draw.circle(surface, self.color, (self.pos_x,self.pos_y) , self.radius)
 
+#create instances of the players and ball
 player1 = Player(1, 20, SCREEN_HEIGHT / 2 - 60, RED)
 player2 = Player(2, SCREEN_WIDTH - 40, SCREEN_HEIGHT / 2 - 60, RED)
 ball = Ball(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, BLUE)
 
 
-
+#main game loop
 while play:
+
+    #keeps game at 60 fps
     clock.tick(60)
 
+    #fills background
     screen.fill(GREEN)
+
+    #draw players and ball
     player1.draw(screen)
     player2.draw(screen)
     ball.draw(screen)
 
+    #move players and ball
     player1.move()
     player2.move()
     ball.move(player1, player2)
 
+    #event handler for pygame events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             play = False
 
+    #updates image for screen
     pygame.display.update()
             
 pygame.quit()
